9c8ffd751a0a0dc18de8962cc4e44c421417d675
sejin\admin <seijin0722@naver.com>
2022-09-17 12:02:53 +0900

    EdgeCase를 고려한 단위테스트 validation 함수로 리팩토링

--- src/main/java/com/sparta/springcore/model/Product.java
+++ src/main/java/com/sparta/springcore/model/Product.java
@@ -1,18 +1,13 @@
 package com.sparta.springcore.model;
-
 import com.sparta.springcore.dto.ProductRequestDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-
-import com.sparta.springcore.dto.ProductRequestDto;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.persistence.*;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 @Setter
 @Getter // get 함수를 일괄적으로 만들어줍니다.
@@ -40,18 +35,69 @@ public class Product {
 
     @Column(nullable = false)
     private int myprice;
-    //회원의 아이디값 저장
+
     @Column(nullable = false)
     private Long userId;
 
-    // 관심 상품 생성 시 이용합니다.
+    // 관심 상품 생성 시 이용합니다. (Edge케이스 처리하는 조건문 추가)
     public Product(ProductRequestDto requestDto, Long userId) {
-         // 관심상품을 등록한 회원 테이블 Id 저장
+        //EdgeCase에 대한 Validation
+        validateProdcutInput(requestDto, userId);
+
+        // 관심상품을 등록한 회원 Id 저장
         this.userId = userId;
         this.title = requestDto.getTitle();
         this.image = requestDto.getImage();
         this.link = requestDto.getLink();
         this.lprice = requestDto.getLprice();
         this.myprice = 0;
+    }
+    // 입력값 Validation
+    /**
+     *  에러를 보면 모두 같은 IllegalArgumentException을 주고 있는데,
+     * 예를 들어 InvalidUserIdException나 InvalidProdcutTitleException으로 만들어 줄 수 있음
+     * 이것에 대한 장점은, 에러마다 다른 성격의 Exception을 만들어줌으로써 구분될 수 있다.
+     * 지금같은 경우의 IllegalArgumentException만 test한다고 가정하면
+     * 테스트코드의 when부분에서 IllegalArgumentException만 실행이 되게되는데
+     * 그러면 title에서도 에러가 날 가능성을 놓치게 된다
+     * 그래서 테스트코드가 성공을 해도 잘못된 테스트이게 됌!!
+     * 그래서 메세지 비교로 에러 위치를 확인하는 방식으로 사용하고 있음
+     */
+    private void validateProdcutInput(ProductRequestDto requestDto, Long userId) {
+        if (userId == null || userId <= 0) {
+            throw new IllegalArgumentException("회원 Id 가 유효하지 않습니다.");
+        }
+
+        if (requestDto.getTitle() == null || requestDto.getTitle().isEmpty()) {
+            throw new IllegalArgumentException("저장할 수 있는 상품명이 없습니다.");
+        }
+
+        if (!isValidUrl(requestDto.getImage())) {
+            throw new IllegalArgumentException("상품 이미지 URL 포맷이 맞지 않습니다.");
+        }
+
+        if (!isValidUrl(requestDto.getLink())) {
+            throw new IllegalArgumentException("상품 최저가 페이지 URL 포맷이 맞지 않습니다.");
+        }
+
+        if (requestDto.getLprice() <= 0) {
+            throw new IllegalArgumentException("상품 최저가가 0 이하입니다.");
+        }
+    }
+
+    boolean isValidUrl(String url)
+    {
+        try {
+            //URL은 url형태로 만들어주는 java.net.class
+            new URL(url).toURI();
+            return true;
+        }
+        //원하던 url패스가 아닐경우 에러
+        catch (URISyntaxException exception) {
+            return false;
+        }
+        catch (MalformedURLException exception) {
+            return false;
+        }
     }
 }
--- src/test/java/com/sparta/springcore/model/ProductTest.java
+++ src/test/java/com/sparta/springcore/model/ProductTest.java
@@ -1,43 +1,333 @@
 package com.sparta.springcore.model;
 
 import com.sparta.springcore.dto.ProductRequestDto;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-class ProductTest { // 클래스 옆 실행 버튼은 클래스 내에 있는 모든 함수를 다 test
-    @Test // Junit으로 test를 돌려주겠다
-    @DisplayName("정상 케이스")
-    void createProduct_Normal() { //함수 옆 실행버튼은 해당 함수에 대한 test
-        // given // 샘플 값 : 이런 환경이 주어졌을 때
-        Long userId = 100L;
-        String title = "오리온 꼬북칩 초코츄러스맛 160g";
-        String image = "https://shopping-phinf.pstatic.net/main_2416122/24161228524.20200915151118.jpg";
-        String link = "https://search.shopping.naver.com/gate.nhn?id=24161228524";
-        int lprice = 2350;
+class ProductTest {
+    /**
+     * Nested : 한 개의 depth 안에서 돌때
+     * 트리처럼 안으로 파고들어서 tab방식으로 테스트코드 만드는법
+     */
+    @Nested
+    @DisplayName("회원이 요청한 관심상품 객체 생성")
+    class CreateUserProduct {
+        /**
+         * 기존에는 Test안에서 given에 변수로 선언해서 사용했었는데
+         * 이번에는 멤버변수로 선언된 이유?
+         * 공통적으로 이 모든 test에서 사용해야하기 때문에 뺐음
+         */
+        private Long userId;
+        private String title;
+        private String image;
+        private String link;
+        private int lprice;
+        /**
+         * BeforeEach : Test를 수행하기 전에
+         * 모든 test에서 한번씩 여기를 거침
+         * 아래는 정상케이스에서 가지고 있었던 샘플예시
+         */
+        @BeforeEach
+        void setup() {
+            userId = 100L;
+            title = "오리온 꼬북칩 초코츄러스맛 160g";
+            image = "https://shopping-phinf.pstatic.net/main_2416122/24161228524.20200915151118.jpg";
+            link = "https://search.shopping.naver.com/gate.nhn?id=24161228524";
+            lprice = 2350;
+        }
 
-        ProductRequestDto requestDto = new ProductRequestDto(
-                title,
-                image,
-                link,
-                lprice
-        );
+        @Test
+        @DisplayName("정상 케이스")
+        /**정상 케이스
+         * 기존 코드에서 멤버변수와 BeforeEach로 빼준것 밖에 없음
+         * 기존 코드 참고해서 확인!
+         */
+        void createProduct_Normal() {
+            /**
+             *  new ProductRequestDto를 만들어 줄때, 멤버변수를 써 가져오는것
+             *  사실은 this.title에서 this를 생략한 것
+             *  setup()함수에서 설정을 해주고 여기로 오는 것이기 때문에 default값으로 설정되어있음
+             */
+            // given
+            ProductRequestDto requestDto = new ProductRequestDto(
+                    title,
+                    image,
+                    link,
+                    lprice
+            );
 
-        // when : 이코드가 실행되면
-        // 생성자를 test해보겠다. 인자에 given넣어줌
-        Product product = new Product(requestDto, userId);
-        //requestDto 인자를 넣어주기 위해서는 ProductRequestDto dto 객체 생성해줘야함
+            // when
+            Product product = new Product(requestDto, userId);
 
-        // then : 실행된 결과
-        //assert:강하게 주장하다
-        assertNull(product.getId());//인자값이 null이여야 한다고 강하게 주장, null이 아닐경우 에러
-        assertEquals(userId, product.getUserId()); // 인자 2개 : 기대값, 실제값
-        assertEquals(title, product.getTitle());
-        assertEquals(image, product.getImage());
-        assertEquals(link, product.getLink());
-        assertEquals(lprice, product.getLprice());
-        assertEquals(0, product.getMyprice());
+            // then
+            assertNull(product.getId());
+            assertEquals(userId, product.getUserId());
+            assertEquals(title, product.getTitle());
+            assertEquals(image, product.getImage());
+            assertEquals(link, product.getLink());
+            assertEquals(lprice, product.getLprice());
+            assertEquals(0, product.getMyprice());
+        }
+
+        @Nested
+        @DisplayName("실패 케이스")
+        class FailCases {
+            @Nested
+            @DisplayName("회원 Id")
+            class userId {
+                @Test
+                @DisplayName("null")//userId가 null경우에 대한 Test
+                void fail1() {
+                    // given : 정상케이스에서 default값을 설정했기 때문에 null로 할당해서 Test
+                    userId = null;
+
+                    ProductRequestDto requestDto = new ProductRequestDto(
+                            title,
+                            image,
+                            link,
+                            lprice
+                    );
+                    /*
+                    Product클래스에서 userId가 null이면 에러를 발생하기로 했는데
+                    애러가 발생하는지 확인하는 테스트 코드
+                    assertThrows:에러가 throws가 돼야한다
+                    조건문에서 만든 IllegalArgumentException가 일치하지 않으면 테스트코드 fail됨
+                     */
+                    // when - then
+                    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                        new Product(requestDto, userId);
+                    });
+                    /*
+                    원래는 then에 모든 것이 모여있어야 하지만,
+                    assertEquals에서 then에 다 모으지 못하고
+                    assertThrows가 실행까지 하기 때문에 when에 위치할 수 밖에 없음
+                     */
+                    /*
+                    expected안에 문자열을 비교하는것은 좋은 test code는 아니다
+                    문자열을 그대로 비교하면, Product클래스에서 조건문의 에러메세지를 변경할때 에러가 나기 때문에
+                    Product클래스에서보면 모두 같은 IllegalArgumentException을 주고 있는데,
+
+
+                     */
+                    // then
+                    assertEquals("회원 Id 가 유효하지 않습니다.", exception.getMessage());
+                }
+
+                @Test
+                @DisplayName("마이너스")
+                void fail2() {
+                    // given
+                    userId = -100L;
+
+                    ProductRequestDto requestDto = new ProductRequestDto(
+                            title,
+                            image,
+                            link,
+                            lprice
+                    );
+
+                    // when
+                    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                        new Product(requestDto, userId);
+                    });
+
+                    // then
+                    assertEquals("회원 Id 가 유효하지 않습니다.", exception.getMessage());
+                }
+            }
+
+            @Nested
+            @DisplayName("상품명")
+            class Title {
+                @Test
+                @DisplayName("null")
+                void fail1() {
+                // given
+                    title = null;
+
+                    ProductRequestDto requestDto = new ProductRequestDto(
+                            title,
+                            image,
+                            link,
+                            lprice
+                    );
+
+                    // when
+                    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                        new Product(requestDto, userId);
+                    });
+
+                    // then
+                    assertEquals("저장할 수 있는 상품명이 없습니다.", exception.getMessage());
+                }
+
+                @Test
+                @DisplayName("빈 문자열")
+                void fail2() {
+                // given
+                    String title = "";
+
+                    ProductRequestDto requestDto = new ProductRequestDto(
+                            title,
+                            image,
+                            link,
+                            lprice
+                    );
+
+                    // when
+                    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                        new Product(requestDto, userId);
+                    });
+
+                    // then
+                    assertEquals("저장할 수 있는 상품명이 없습니다.", exception.getMessage());
+                }
+            }
+
+            @Nested
+            @DisplayName("상품 이미지 URL")
+            class Image {
+                @Test
+                @DisplayName("null")
+                void fail1() {
+                    // given
+                    image = null;
+
+                    ProductRequestDto requestDto = new ProductRequestDto(
+                            title,
+                            image,
+                            link,
+                            lprice
+                    );
+
+                    // when
+                    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                        new Product(requestDto, userId);
+                    });
+
+                    // then
+                    assertEquals("상품 이미지 URL 포맷이 맞지 않습니다.", exception.getMessage());
+                }
+
+                @Test
+                @DisplayName("URL 포맷 형태가 맞지 않음")
+                void fail2() {
+                    // given
+                    image = "shopping-phinf.pstatic.net/main_2416122/24161228524.20200915151118.jpg";
+
+                    ProductRequestDto requestDto = new ProductRequestDto(
+                            title,
+                            image,
+                            link,
+                            lprice
+                    );
+
+                    // when
+                    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                        new Product(requestDto, userId);
+                    });
+
+                    // then
+                    assertEquals("상품 이미지 URL 포맷이 맞지 않습니다.", exception.getMessage());
+                }
+            }
+
+            @Nested
+            @DisplayName("상품 최저가 페이지 URL")
+            class Link {
+                @Test
+                @DisplayName("null")
+                void fail1() {
+                    // given
+                    link = "https";
+
+                    ProductRequestDto requestDto = new ProductRequestDto(
+                            title,
+                            image,
+                            link,
+                            lprice
+                    );
+
+                    // when
+                    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                        new Product(requestDto, userId);
+                    });
+
+                    // then
+                    assertEquals("상품 최저가 페이지 URL 포맷이 맞지 않습니다.", exception.getMessage());
+                }
+
+                @Test
+                @DisplayName("URL 포맷 형태가 맞지 않음")
+                void fail2() {
+                   // given
+                    link = "https";
+
+                    ProductRequestDto requestDto = new ProductRequestDto(
+                            title,
+                            image,
+                            link,
+                            lprice
+                    );
+
+                     // when
+                    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                        new Product(requestDto, userId);
+                    });
+
+                     // then
+                    assertEquals("상품 최저가 페이지 URL 포맷이 맞지 않습니다.", exception.getMessage());
+                }
+            }
+
+            @Nested
+            @DisplayName("상품 최저가")
+            class LowPrice {
+                @Test
+                @DisplayName("0")
+                void fail1() {
+                    // given
+                    lprice = 0;
+
+                    ProductRequestDto requestDto = new ProductRequestDto(
+                            title,
+                            image,
+                            link,
+                            lprice
+                    );
+
+                    // when
+                    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                        new Product(requestDto, userId);
+                    });
+
+                    // then
+                    assertEquals("상품 최저가가 0 이하입니다.", exception.getMessage());
+                }
+
+                @Test
+                @DisplayName("음수")
+                void fail2() {
+                    // given
+                    lprice = -1500;
+
+                    ProductRequestDto requestDto = new ProductRequestDto(
+                            title,
+                            image,
+                            link,
+                            lprice
+                    );
+
+                    // when
+                    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+                        new Product(requestDto, userId);
+                    });
+
+                    // then
+                    assertEquals("상품 최저가가 0 이하입니다.", exception.getMessage());
+                }
+            }
+        }
     }
 }
